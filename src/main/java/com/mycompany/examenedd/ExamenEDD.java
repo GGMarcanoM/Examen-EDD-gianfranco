0681cd79315e6bd64b4f82524987e499ffe834ff
Gian <gmarcano.8807@unimar.edu.ve>
2024-07-16 22:18:55 -0400

    ejercicio1

--- src/main/java/com/mycompany/examenedd/ExamenEDD.java
+++ src/main/java/com/mycompany/examenedd/ExamenEDD.java
@@ -1,27 +1,25 @@
-
-
 package com.mycompany.examenedd;
+
 import java.util.Scanner;
 
 public class ExamenEDD {
-
-  public static void int sumarecursiva(int[] arreglo, int i)
-            
-            
-    public static void main(String[] args) {
-        System.out.println("Ingrese el tamanio del array");
-        Scanner sc = new Scanner(System.in);
-       int tam = sc.nextInt();
-       
-       int[] arreglo = new int[tam];
-       for (int i = 0; i < tam; i++)
-       { 
-           System.out.println("agregar valor" + i +" ");
-           arreglo[i] = sc.nextInt();
-       }
-
-    
-        
+    public static void main(String[] args){
+     
+        Scanner sc = new Scanner(System.in); 
+        System.out.print("tamanyo del array: ");
+        int dat = sc.nextInt();
+        int[] Arr = new int[dat];
+        for (int i = 0; i < dat; i++) {
+            System.out.print("valor de puesto " + i + ": ");
+            Arr[i] = sc.nextInt();
+            int suma = sumaElementos(Arr, dat - 1);
+            System.out.println("  ");
+        System.out.println("La suma de los elementos del array es: " + suma);
+        }
+        }
+    public static int sumaElementos(int[] arr, int guia) {
+        if (guia < 0) return 0;
+        return arr[guia] + sumaElementos(arr, guia - 1);
     }
 }
 
