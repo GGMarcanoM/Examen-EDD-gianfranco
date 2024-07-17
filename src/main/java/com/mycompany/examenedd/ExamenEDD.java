897a530e4370546e846f78ed7040f515308d573a
Gian <gmarcano.8807@unimar.edu.ve>
2024-07-16 21:15:42 -0400

    subiendo la carpeta

---
+++ src/main/java/com/mycompany/examenedd/ExamenEDD.java
@@ -0,0 +1,27 @@
+
+
+package com.mycompany.examenedd;
+import java.util.Scanner;
+
+public class ExamenEDD {
+
+  public static void int sumarecursiva(int[] arreglo, int i)
+            
+            
+    public static void main(String[] args) {
+        System.out.println("Ingrese el tamanio del array");
+        Scanner sc = new Scanner(System.in);
+       int tam = sc.nextInt();
+       
+       int[] arreglo = new int[tam];
+       for (int i = 0; i < tam; i++)
+       { 
+           System.out.println("agregar valor" + i +" ");
+           arreglo[i] = sc.nextInt();
+       }
+
+    
+        
+    }
+}
+
